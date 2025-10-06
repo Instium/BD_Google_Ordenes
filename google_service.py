b5876de27d9ed3b12e81e25b85708fd06e3e3910
Instium <instium15@gmail.com>
2025-10-06 12:59:19 -0600

    Version final

--- google_service.py
+++ google_service.py
@@ -8,15 +8,11 @@ def get_sheet():
         "https://www.googleapis.com/auth/drive"
     ]
 
-    # ✅ Lee directamente los valores de la sección [google] del secrets.toml
-    creds_dict = dict(st.secrets["google"])
-
-    # 🔑 Autoriza con las credenciales
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-    client = gspread.authorize(creds)
-
-    # 📄 Abre la hoja
-    sheet = client.open("Ordenes_BackOffice").sheet1
+    with st.spinner("🔄 Conectando con Google Sheets..."):
+        creds_dict = dict(st.secrets["google"])
+        creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+        client = gspread.authorize(creds)
+        sheet = client.open("Ordenes_BackOffice").sheet1
 
     st.toast("✅ Conectado correctamente a Google Sheets", icon="🟢")
     return sheet
